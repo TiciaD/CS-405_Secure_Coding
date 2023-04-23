140ab9f5439094809b9c9969621c9e8bc8fce539
TiciaD <dunnticia633@gmail.com>
2023-04-23 16:59:59 -0400

    Add CPP files for each coding assignment

---
+++ Secure_Coding_Assignments/CS-405_Module_Four_Exceptions.cpp
@@ -0,0 +1,102 @@
+// Exceptions.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+// custom Exception class that is derived from the std::exception class
+struct Exception : public std::exception
+{
+    virtual const char* what() const throw()
+    {
+        return "Hello from custom exception!";
+    }
+};
+
+bool do_even_more_custom_application_logic()
+{
+    // Throw any standard exception
+    throw std::bad_exception();
+
+    std::cout << "Running Even More Custom Application Logic." << std::endl;
+
+    return true;
+}
+void do_custom_application_logic()
+{
+    //  Wrap the call to do_even_more_custom_application_logic()
+    //  with an exception handler that catches std::exception, displays
+    //  a message and the exception.what(), then continues processing
+    std::cout << "Running Custom Application Logic." << std::endl;
+
+    try {
+        if (do_even_more_custom_application_logic())
+        {
+            std::cout << "Even More Custom Application Logic Succeeded." << std::endl;
+        }
+    }
+    catch (const std::exception &exception) {
+        std::cerr << "ERROR: " << exception.what() << std::endl;
+    }
+
+    // Throw a custom exception derived from std::exception
+    //  and catch it explictly in main
+    throw Exception();
+
+    std::cout << "Leaving Custom Application Logic." << std::endl;
+
+}
+
+float divide(float num, float den)
+{
+    // Throw an exception to deal with divide by zero errors using
+    //  a standard C++ defined exception
+    if (den == 0)
+    {
+        throw std::runtime_error("Divide by Zero is illegal");
+    }
+    return (num / den);
+}
+
+void do_division() noexcept
+{
+    //  create an exception handler to capture ONLY the exception thrown
+    //  by divide.
+
+    float numerator = 10.0f;
+    float denominator = 0;
+
+    try {
+        auto result = divide(numerator, denominator);
+        std::cout << "divide(" << numerator << ", " << denominator << ") = " << result << std::endl;
+    }
+    catch (const std::exception &exception) {
+        std::cerr << "ERROR: " << exception.what() << std::endl;
+    }
+}
+
+int main()
+{
+    std::cout << "Exceptions Tests!" << std::endl;
+
+    //  Create exception handlers that catch (in this order):
+    //  your custom exception
+    //  std::exception
+    //  uncaught exception 
+    //  that wraps the whole main function, and displays a message to the console.
+    try {
+        do_division();
+        do_custom_application_logic();
+    }
+    catch (const Exception &exception) {
+        std::cerr << "ERROR: " << exception.what() << std::endl;
+    }
+    catch (const std::exception &exception) {
+        std::cerr << "ERROR: " << exception.what() << std::endl;
+    }
+    catch (...) {
+        std::cerr << "ERROR: Unknown Exception" << std::endl;
+    }
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
---
+++ Secure_Coding_Assignments/CS-405_Module_Two_Buffer_Overflow.cpp
@@ -0,0 +1,31 @@
+// BufferOverflow.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iomanip>
+#include <iostream>
+
+int main()
+{
+	std::cout << "Buffer Overflow Example" << std::endl;
+
+	// TODO: The user can type more than 20 characters and overflow the buffer, resulting in account_number being replaced -
+	//  even though it is a constant and the compiler buffer overflow checks are on.
+	//  You need to modify this method to prevent buffer overflow without changing the account_order
+	//  varaible, and its position in the declaration. It must always be directly before the variable used for input.
+
+	const std::string account_number = "CharlieBrown42";
+	char user_input[20];
+	std::cout << "Enter a value: ";
+	// getline reads characters from an input stream and places them into a string, can add an optional delimeter
+	std::cin.getline(user_input, 20);
+	// If the input stream fails, which it will if the input is larger than the delimeter
+	if (std::cin.fail()) {
+		// Output error message
+		std::cout << "ERROR - Potential Buffer Overflow! Limiting input to the first 20 characters" << std::endl;
+	}
+	std::cout << "You entered: " << user_input << std::endl;
+	std::cout << "Account Number = " << account_number << std::endl;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
